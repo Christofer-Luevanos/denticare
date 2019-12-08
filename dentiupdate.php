6620b027e665c7c7495c05644e38a23e89cf21f0
Christofer Luevanos <a17100195@gmail.com>
2019-12-08 16:19:37 -0600

    tercer commit

--- dentiupdate.php
+++ dentiupdate.php
@@ -6,10 +6,9 @@
   $gender = filter_input(INPUT_POST, "sexo");
   $phone = filter_input(INPUT_POST, "telefono");
   $address = filter_input(INPUT_POST, "direccion");
-  $query2 = "SELECT * FROM paciente WHERE ID_PAC = '".$lastId."'";
+  $query2 = "SELECT ID_PAC FROM paciente WHERE NOMBRE_P LIKE '".$nombre."'";
   $result2 = mysqli_query($conn, $query2);
-  $query = "UPDATE PACIENTE SET NOMBRE_P='".$name."',APELLIDO_P='".$lastname."',SEXO='".$gender."',TELEFONO='".$phone."',DOMICILIO='".$address."',FECHA_NAC='".$birthday."'
-  WHERE ID_PAC='".$result2."'";
+  $query = "UPDATE PACIENTE SET NOMBRE_P='".$name."',APELLIDO_P='".$lastname."',SEXO='".$gender."',TELEFONO='".$phone."',DOMICILIO='".$address."',FECHA_NAC='".$birthday."'WHERE ID_PAC='".$result2."'";
   $result = mysqli_query($conn, $query);
   $lastId = mysqli_insert_id($conn);
   $row = mysqli_fetch_array($result2);
