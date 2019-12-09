1b9d6c2ad34f8fbd584a035b6f75402a32dbb2b2
Christofer Luevanos <a17100195@gmail.com>
2019-12-09 03:51:05 -0600

    denticastigodivino

--- dentipacient.php
+++ dentipacient.php
@@ -1,11 +1,11 @@
 <?php
 header('Content-Type: application/json');
 include('denticonfig.php');
-$query = "SELECT * FROM paciente";
+$query = "SELECT * FROM paciente ";
 $resultQuery = mysqli_query($conn, $query);
 $data = array();
 foreach($resultQuery as $row){
-  $data[] = $row;
+  $data["results"][] = $row;
 }
 
 mysqli_close($conn);
