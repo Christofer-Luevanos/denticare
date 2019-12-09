a19b021a3d2a369d5968e5d68edf68bf5d0e59e7
Christofer Luevanos <a17100195@gmail.com>
2019-12-08 22:14:50 -0600

    ultimo intento

--- dentiupdate.php
+++ dentiupdate.php
@@ -1,21 +1,15 @@
 <?php
   include 'denticonfig.php';
-  //$name = filter_input(INPUT_POST, "nombre");
-  //$lastname = filter_input(INPUT_POST, "apellido");
-  //$birthday = filter_input(INPUT_POST, "nacimiento");
-  //$gender = filter_input(INPUT_POST, "sexo");
-  //$phone = filter_input(INPUT_POST, "telefono");
-  //$address = filter_input(INPUT_POST, "direccion");
-  $name = "Christofer";
-  $lastname = "Luevanos";
-  $birthday = "05/Octubre/2002";
-  $gender = "HOMBRE";
-  $phone = "3316959444";
-  $address = "Av Torremolinos";
-  $query2 = "SELECT ID_PAC FROM paciente WHERE NOMBRE_P LIKE '".$name."'";
+  $name = filter_input(INPUT_POST, "nombre");
+  $lastname = filter_input(INPUT_POST, "apellido");
+  $birthday = filter_input(INPUT_POST, "nacimiento");
+  $gender = filter_input(INPUT_POST, "sexo");
+  $phone = filter_input(INPUT_POST, "telefono");
+  $address = filter_input(INPUT_POST, "direccion");
+  $query2 = "SELECT ID_PAC FROM paciente WHERE NOMBRE_P ='".$name."'";
   $result2 = mysqli_query($conn, $query2);
   $arrayid=mysqli_fetch_array($result2);
-  $query = "UPDATE PACIENTE SET NOMBRE_P='".$name."',APELLIDO_P='".$lastname."',SEXO='".$gender."',TELEFONO='".$phone."',DOMICILIO='".$address."',FECHA_NAC='".$birthday."'WHERE ID_PAC='".$arrayid[0]."'";
+  $query = "UPDATE PACIENTE SET NOMBRE_P='".$name."',APELLIDO_P='".$lastname."',SEXO='".$gender."',TELEFONO='".$phone."',DOMICILIO='".$address."',FECHA_NAC='".$birthday."'WHERE ID_PAC='".$arrayid[2]."'";
   $result = mysqli_query($conn, $query);
   $lastId = mysqli_insert_id($conn);
 
